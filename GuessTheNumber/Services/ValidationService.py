020806488ff138ef03e6d4f61a334c628c18bb74
Luciano Fraschini <30655552+LuciooF@users.noreply.github.com>
2020-07-03 16:50:37 +0100

    made validation function shorter (One line big brain)

--- GuessTheNumber/Services/ValidationService.py
+++ GuessTheNumber/Services/ValidationService.py
@@ -1,5 +1,2 @@
-def ValidateUserInfo(userInput):
-    if userInput > 10 or userInput < 0:
-        return False #does this return anything ? so in this case a bool but if i wanted its an int? in csharp yiou have to name it
-    else: 
-        return True
+def ValidateUserInfo(userInput):    
+    return (userInput < 10 or userInput > 0)
